0fde4ea44963d0602b52c71abb3242602f1b1d46
Tatsu88-Tokyo <tatsuhiko1028@gmail.com>
2020-03-14 00:26:55 +0900

    マイページ　マークアップ#1

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,12 @@ Rails.application.routes.draw do
   }
   root "home#top"
   get 'home/registration'
+  resources :user, only: [:show, :edit, :update]do
+    member do
+      get 'profile'
+      patch 'profile_update'
+      get 'logout'
+    end
+  end
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
