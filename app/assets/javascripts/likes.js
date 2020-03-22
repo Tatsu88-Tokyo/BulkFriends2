90af1291eb4d2105facfebe3bdb9f3ba5ad907cc
Tatsu88-Tokyo <tatsuhiko1028@gmail.com>
2020-03-22 17:51:39 +0900

    いいねボタン実装（view,controller)

--- app/assets/javascripts/likes.js
+++
@@ -1,7 +0,0 @@
-// $("#follow_form").html(`<div class="btn-liked"></div>`)
-
-$(function() {
-  $("#follow_form").click(function() {
-    $("#follow_form").html('<div class="btn-liked"><i class="fas fa-heart fa-3x"></i></div>')
-  });
-});
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def top
+    if user_signed_in?
+      @user = current_user
+      redirect_to user_path(@user.id)
+    end
   end
 end
