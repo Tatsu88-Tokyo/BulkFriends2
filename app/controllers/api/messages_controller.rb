38f1ee7292a7e31166d2351b3ba4e0f2276e087a
Tatsu88-Tokyo <tatsuhiko1028@gmail.com>
2020-04-02 00:45:32 +0900

    自動更新編集（未実装）

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,18 +1,12 @@
 class Api::MessagesController < ApplicationController
   def index
-    user = User.find(params[:id])
+    @text = Message.where(user_id:current_user,receive_user_id:params[:id])
     # ajaxで送られてくる最後のメッセージのid番号を変数に代入
-    last_message_id = params[:id].to_i
+    last_message_id = @text.ids.last
+    @content = @text.find(last_message_id)
+    user = User.find(@content.user_id)
     # 取得したグループでのメッセージ達から、idがlast_message_idよりも新しい(大きい)メッセージ達のみを取得
-    @messages = user.messages.includes(:user).where("id > ?", last_message_id)
+    @messages = user.messages.where("id > #{last_message_id}")
   end
 end
 
-# def index
-#   # user = User.find(params[:id])
-#   @message = Message.where(user_id:current_user,receive_user_id:params[:id])
-#   # ajaxで送られてくる最後のメッセージのid番号を変数に代入
-#   last_message_id = @message.ids.last
-#   # 取得したグループでのメッセージ達から、idがlast_message_idよりも新しい(大きい)メッセージ達のみを取得
-#   @messages = user.messages.includes(:user).where("id > ?", last_message_id)
-# end
