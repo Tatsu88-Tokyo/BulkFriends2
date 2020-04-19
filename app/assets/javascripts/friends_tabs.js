dcf42d1f836b47f1214e61cdc2326bd7466064e2
Tatsu88-Tokyo <tatsuhiko1028@gmail.com>
2020-04-19 23:07:48 +0900

    Update friends_tabs.js

--- app/assets/javascripts/friends_tabs.js
+++ app/assets/javascripts/friends_tabs.js
@@ -1,7 +1,7 @@
 $(function() {
   //クリックしたときのファンクションをまとめて指定
   $('.tab li').click(function() {
-      const index = $('.tab li').index(this);
+      let index = $('.tab li').index(this);
       $('.content li').css('display','none');
       $('.content li').eq(index).css('display','block');
       $('.tab li').removeClass('select');
